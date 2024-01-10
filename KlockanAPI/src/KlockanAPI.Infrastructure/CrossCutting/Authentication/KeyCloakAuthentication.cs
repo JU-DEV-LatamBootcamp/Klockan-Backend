54a582b75b9d2b93430762f6f57bf2d100970bb5
RSarabiaJala <rudy.sarabia@jala.university>
2024-01-10 00:12:02 -0400

    #9 - Added KeyCloak Authentication module to Infrastructure Layer

---
+++ KlockanAPI/src/KlockanAPI.Infrastructure/CrossCutting/Authentication/KeyCloakAuthentication.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Hosting;
+
+namespace KlockanAPI.Infrastructure.CrossCutting.Authentication
+{
+    public static class KeyCloakAuthentication
+    {
+        public static AuthenticationBuilder AddKeyCloakJWTAuthentication(this IServiceCollection services, IConfigurationSection KeyCloakSecrets, IHostEnvironment env)
+        {
+            return services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(o =>
+            {
+                o.RequireHttpsMetadata = env.IsProduction();
+                o.Authority = KeyCloakSecrets["Authority"];
+                o.Audience = KeyCloakSecrets["Audience"];
+                o.Events = new JwtBearerEvents()
+                {
+                    OnAuthenticationFailed = c =>
+                    {
+                        c.NoResult();
+                        c.Response.StatusCode = 500;
+                        c.Response.ContentType = "text/plain";
+                        if (env.IsDevelopment())
+                        {
+                            return c.Response.WriteAsJsonAsync(c.Exception.ToString());
+                        }
+                        return c.Response.WriteAsJsonAsync("An error occured processing your authentication.");
+                    }
+                };
+            });
+        }
+    }
+}
