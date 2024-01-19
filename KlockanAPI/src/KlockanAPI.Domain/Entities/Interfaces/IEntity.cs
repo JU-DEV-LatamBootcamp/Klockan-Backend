e9df1ba31a80b5bbd5f2b3fafdf71ad459b8ebed
Andres J. Nazzari <andres.nazzari@jala.university>
2024-01-19 19:22:24 -0300

    Delete Course, IEntity, and Program classes TG-42

--- KlockanAPI/src/KlockanAPI.Domain/Entities/Interfaces/IEntity.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace KlockanAPI.Domain.Entities.Interfaces;
-public interface IEntity {
-    int Id { get; set; }
-    DateTime CreatedAt { get; set; }
-    DateTime UpdatedAt { get; set; }
-    DateTime DeletedAt { get; set; }
-}
