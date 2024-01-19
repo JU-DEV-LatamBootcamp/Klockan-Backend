b9edf18f41f42245af03d93c6ee4ffb9307ba3d2
Andres J. Nazzari <andres.nazzari@jala.university>
2024-01-19 12:29:51 -0300

    Add Program entity class TG-42

---
+++ KlockanAPI/src/KlockanAPI.Domain/Entities/Program.cs
@@ -0,0 +1,19 @@
+﻿using KlockanAPI.Domain.Entities.Interfaces;
+
+namespace KlockanAPI.Domain.Entities;
+public class Program : IEntity {
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public DateTime DeletedAt { get; set; }
+
+    public Program(int id, string name, string description) {
+        this.Id = id;
+        this.Name = name;
+        this.Description = description;
+    }
+}
+
