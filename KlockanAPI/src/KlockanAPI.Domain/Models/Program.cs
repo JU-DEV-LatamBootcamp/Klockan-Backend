0aed3b6a5c66d1b0d34f500bb705bff44a140738
Andres J. Nazzari <andres.nazzari@jala.university>
2024-01-19 12:30:04 -0300

    Add Program class to KlockanAPI.Domain.Models TG-42

---
+++ KlockanAPI/src/KlockanAPI.Domain/Models/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace KlockanAPI.Domain.Models;
+
+public class Program {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    public Program(int id, string name, string description) {
+        this.Id = id;
+        this.Name = name;
+        this.Description = description;
+    }
+
+}
