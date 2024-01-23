8b9b8d191904f36d06abda66a7620d0ec8bec460
Andres J. Nazzari <andres.nazzari@jala.university>
2024-01-23 14:26:58 -0300

    Refactor Role class to include additional properties  TG-42

--- KlockanAPI/src/KlockanAPI.Domain/Models/Role.cs
+++ KlockanAPI/src/KlockanAPI.Domain/Models/Role.cs
@@ -1,7 +1,8 @@
 namespace KlockanAPI.Domain.Models;
 
-public class Role(string name) : BaseModel
+public class Role : BaseModel
 {
-    public string Name { get; set; } = name;
-
+    public string Name { get; set; } = string.Empty;
+    public ICollection<User> Users { get; set; } = new List<User>();
+    public ICollection<ClassroomUser> ClassroomUsers { get; set; } = new List<ClassroomUser>();
 }
