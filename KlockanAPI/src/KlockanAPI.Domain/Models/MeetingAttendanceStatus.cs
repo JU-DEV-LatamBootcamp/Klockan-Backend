2edb4cab99f720a749dc15e85827536323a8c2b7
Andres J. Nazzari <andres.nazzari@jala.university>
2024-01-23 14:26:48 -0300

    Refactor MeetingAttendanceStatus class  TG-42

--- KlockanAPI/src/KlockanAPI.Domain/Models/MeetingAttendanceStatus.cs
+++ KlockanAPI/src/KlockanAPI.Domain/Models/MeetingAttendanceStatus.cs
@@ -1,6 +1,7 @@
 namespace KlockanAPI.Domain.Models;
 
-public class MeetingAttendanceStatus(string name) : BaseModel
+public class MeetingAttendanceStatus : BaseModel
 {
-    public string Name { get; set; } = name;
+    public string Name { get; set; } = string.Empty;
+    public ICollection<MeetingAttendance> MeetingAttendances { get; set; } = new List<MeetingAttendance>();
 }
