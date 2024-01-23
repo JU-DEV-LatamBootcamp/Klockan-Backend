cfde6b0a7d6401b927ae40bf5f1b1ce8c49d528a
Andres J. Nazzari <andres.nazzari@jala.university>
2024-01-23 14:26:02 -0300

    Refactor Classroom model to use ICollection and initialize properties  TG-42

--- KlockanAPI/src/KlockanAPI.Domain/Models/Classroom.cs
+++ KlockanAPI/src/KlockanAPI.Domain/Models/Classroom.cs
@@ -1,22 +1,13 @@
 namespace KlockanAPI.Domain.Models;
 
-public class Classroom(
-    Course course,
-    DateOnly startDate,
-    User creator,
-    Program program,
-    Meeting[] meetings,
-    ClassroomUser[] classroomUsers,
-    Schedule schedule
-) : BaseModel
+public class Classroom: BaseModel
 {
-
-    public Course Course { get; set; } = course;
-    public DateOnly StartDate { get; set; } = startDate;
-    public User Creator { get; set; } = creator;
-    public Program Program { get; set; } = program;
-    public Meeting[]? Meetings { get; set; } = meetings;
-    public ClassroomUser[]? ClassroomUsers { get; set; } = classroomUsers;
-    public Schedule? Schedule { get; set; } = schedule;
-
+    public DateOnly StartDate { get; set; } 
+    public int CourseId { get; set; }
+    public Course Course { get; set; } = new Course();
+    public int ProgramId { get; set;}
+    public Program Program { get; set; } = new Program();
+    public ICollection<Meeting> Meetings { get; set; } = new List<Meeting>();
+    public ICollection<Schedule> Schedule { get; set; } = new List<Schedule>();
+    public ICollection<ClassroomUser> ClassroomUsers { get; set; } = new List<ClassroomUser>();
 }
