2ac8a2ee7427c917f9a6593046e8a57756dd83c8
Andres J. Nazzari <andres.nazzari@jala.university>
2024-01-23 14:26:34 -0300

    Add Holiday model class TG-42

--- KlockanAPI/src/KlockanAPI.Domain/Models/Hollyday.cs
+++
@@ -1,8 +0,0 @@
-namespace KlockanAPI.Domain.Models;
-
-public class Hollyday(DateOnly date, string description) : BaseModel
-{
-    public DateOnly Date { get; set; } = date;
-    public string Description { get; set; } = description;
-
-}
