8d10157c2b819b181c378be7ac3cb7644ab46ff5
Andres J. Nazzari <andres.nazzari@jala.university>
2024-01-23 22:07:55 -0300

    Refactor API configuration and add versioning, compression, and health checks TG-60

--- KlockanAPI/src/KlockanAPI.Presentation/Program.cs
+++ KlockanAPI/src/KlockanAPI.Presentation/Program.cs
@@ -1,12 +1,15 @@
 
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Asp.Versioning;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
+using KlockanAPI.Infrastructure;
 using KlockanAPI.Infrastructure.Data;
 using KlockanAPI.Infrastructure.CrossCutting.Authentication;
+using KlockanAPI.Application;
 
 namespace KlockanAPI.Presentation
 {
@@ -16,13 +19,68 @@ namespace KlockanAPI.Presentation
         {
             var builder = WebApplication.CreateBuilder(args);
             // Add services to the container.
+            builder = ConfigureServicesAndMiddlewares(builder);
 
-            builder.Services.AddControllers();
+
+            var app = builder.Build();
+            app.UseResponseCompression();
+            // Configure the HTTP request pipeline.
+            if(app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseCors(c =>
+            {
+                var allowedOrigins = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>();
+                if(allowedOrigins is not null)
+                    c.WithOrigins(allowedOrigins).AllowAnyHeader().AllowAnyMethod();
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllers();
+            app.MapHealthChecks("/health");
+            app.Run();
+        }
+
+        public static WebApplicationBuilder ConfigureServicesAndMiddlewares(WebApplicationBuilder builder)
+        {
+            // ***********  API CONTROLLERS AND RESPONSES ************
+            builder.Services.AddControllers(configure =>
+            {
+                configure.ReturnHttpNotAcceptable = true;
+                configure.Filters.Add(new ProducesResponseTypeAttribute(StatusCodes.Status400BadRequest));
+                configure.Filters.Add(new ProducesResponseTypeAttribute(StatusCodes.Status406NotAcceptable));
+                configure.Filters.Add(new ProducesResponseTypeAttribute(StatusCodes.Status500InternalServerError));
+                configure.Filters.Add(new AuthorizeFilter());
+            });
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
 
+            // ***********  API SWAGGER GEN ************
             builder.Services.AddSwaggerGen(c =>
             {
+                // General information
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "Klockan API",
+                    Version = "v1",
+                    Description = "This API lets you access to Klockan Project.",
+                    Contact = new()
+                    {
+                        Name = "Klockan Project",
+                        Email = "klockanporject@gmail.com"
+                    },
+                    License = new()
+                    {
+                        Name = "MIT License",
+                    },
+                });
+
                 // Keycloak
                 c.AddSecurityDefinition("KeyCloakBearerAuth", new OpenApiSecurityScheme
                 {
@@ -47,34 +105,48 @@ namespace KlockanAPI.Presentation
                 c.AddSecurityRequirement(securityRequirement);
             });
 
+            // ***********  API VERSIONING ************
+            var apiVersioningBuilder = builder.Services.AddApiVersioning(setupAction =>
+            {
+                setupAction.AssumeDefaultVersionWhenUnspecified = true;
+                setupAction.DefaultApiVersion = new ApiVersion(1, 0);
+                setupAction.ReportApiVersions = true;
+
+            });
+            apiVersioningBuilder.AddApiExplorer(options =>
+            {
+                // add the versioned api explorer, which also adds IApiVersionDescriptionProvider service
+                // note: the specified format code will format the version as "'v'major[.minor][-status]"
+                options.GroupNameFormat = "'v'VVV";
+
+                // note: this option is only necessary when versioning by url segment. the SubstitutionFormat
+                // can also be used to control the format of the API version in route templates
+                options.SubstituteApiVersionInUrl = true;
+            });
+
+            // ***********  GZIP COMPRESSION ************
+            builder.Services.AddResponseCompression(options =>
+            {
+                options.EnableForHttps = true;
+                options.Providers.Add<GzipCompressionProvider>();
+            });
+
+            // ***********  KEYCLOAK ************
             builder.Services.AddKeyCloakJWTAuthentication(builder.Configuration.GetSection("KeyCloakJwt"), builder.Environment);
 
+            // ***********  DEPENDENCY INJECTION ************
+            builder.Services.AddApplicationServices();
+            builder.Services.AddInfraestructureRepositories();
+
+            // ***********  HEALTHCHECK ************
+            builder.Services.AddHealthChecks();
+
+            // ***********  DBCONTEXT ************
             builder.Services.AddDbContext<KlockanContext>(options =>
                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-            app.UseCors(c =>
-            {
-                var allowedOrigins = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>();
-                if (allowedOrigins is not null)
-                    c.WithOrigins(allowedOrigins).AllowAnyHeader().AllowAnyMethod();
-            });
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.Run();
+            return builder;
         }
     }
 }
+
