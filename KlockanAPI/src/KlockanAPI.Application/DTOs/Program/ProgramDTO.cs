f8833cb9c9eb9478ccb790fb3b9bba8999475892
Andres J. Nazzari <andres.nazzari@jala.university>
2024-01-23 22:06:28 -0300

    Add ProgramDTO class  TG-60

---
+++ KlockanAPI/src/KlockanAPI.Application/DTOs/Program/ProgramDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace KlockanAPI.Application.DTOs.Program;
+
+public class ProgramDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+}
+
