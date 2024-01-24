a416e022af76db1ca4723830e82e30920bee65e3
Andres J. Nazzari <andres.nazzari@jala.university>
2024-01-24 11:23:23 -0300

    Remove unused code from ProgramRepository TG-60

--- KlockanAPI/src/KlockanAPI.Infrastructure/Repositories/ProgramRepository.cs
+++ KlockanAPI/src/KlockanAPI.Infrastructure/Repositories/ProgramRepository.cs
@@ -1,7 +1,7 @@
 ﻿using KlockanAPI.Domain.Models;
-using KlockanAPI.Application.Services.Interfaces;
 using Microsoft.AspNetCore.Http;
 using KlockanAPI.Infrastructure.Data;
+using KlockanAPI.Infrastructure.Repositories.Interfaces;
 
 namespace KlockanAPI.Infrastructure.Repositories;
 public class ProgramRepository : IProgramRepository
@@ -17,27 +17,5 @@ public class ProgramRepository : IProgramRepository
     {
         return await Task.FromResult(_context.Programs.ToList());
 
-        // eliminar lo de aca abajo
-        var programs = new List<Program>()
-        {
-            new Program
-            {
-                Id = 1,
-                Name = "Bootcamp Developers 01",
-                Description = "Program covering concepts in software development.",
-                CreatedAt = new DateTime(2024, 1, 23, 0, 0, 0, DateTimeKind.Utc),
-                UpdatedAt = new DateTime(2024, 1, 23, 0, 0, 0, DateTimeKind.Utc)
-            },
-            new Program
-            {
-                Id = 2,
-                Name = "Advanced Bootcamp Developers 01",
-                Description = "Program focused on advanced software design and development techniques.",
-                CreatedAt = new DateTime(2024, 1, 23, 0, 0, 0, DateTimeKind.Utc),
-                UpdatedAt = new DateTime(2024, 1, 23, 0, 0, 0, DateTimeKind.Utc)
-            },
-        };
-
-        return await Task.FromResult(programs.ToList());
     }
 }
