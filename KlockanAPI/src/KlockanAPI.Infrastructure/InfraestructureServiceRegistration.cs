7bc69ea7fa1f34c22a5ed5913f82dde0a092bcbe
Andres J. Nazzari <andres.nazzari@jala.university>
2024-01-23 22:07:46 -0300

    Add infrastructure service registration TG-60

---
+++ KlockanAPI/src/KlockanAPI.Infrastructure/InfraestructureServiceRegistration.cs
@@ -0,0 +1,18 @@
+﻿using KlockanAPI.Infrastructure.Repositories;
+using KlockanAPI.Application.Services.Interfaces;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KlockanAPI.Infrastructure;
+
+public static class InfraestructureServiceRegistration
+{
+    public static IServiceCollection AddInfraestructureRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IProgramRepository, ProgramRepository>();
+
+        return services;
+    }
+}
+
+
