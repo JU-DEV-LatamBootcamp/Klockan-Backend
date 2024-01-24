3d7a0f302dba140025458f14c783f8bbb9ad809b
Andres J. Nazzari <andres.nazzari@jala.university>
2024-01-24 11:23:39 -0300

    Add IProgramRepository interface TG-60

---
+++ KlockanAPI/src/KlockanAPI.Infrastructure/Repositories/Interfaces/IProgramRepository.cs
@@ -0,0 +1,8 @@
+﻿using KlockanAPI.Domain.Models;
+
+namespace KlockanAPI.Infrastructure.Repositories.Interfaces;
+
+public interface IProgramRepository
+{
+    public Task<List<Program>> GetProgramsAsync();
+}
