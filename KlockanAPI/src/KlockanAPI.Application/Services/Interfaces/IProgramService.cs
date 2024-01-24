30cd19df3cf6d240a0c6222956eff74b2aa40377
Andres J. Nazzari <andres.nazzari@jala.university>
2024-01-23 22:07:31 -0300

    Add IProgramService and ProgramService implementations TG-60

---
+++ KlockanAPI/src/KlockanAPI.Application/Services/Interfaces/IProgramService.cs
@@ -0,0 +1,8 @@
+﻿using KlockanAPI.Application.DTOs.Program;
+
+namespace KlockanAPI.Application.Services.Interfaces;
+
+public interface IProgramService
+{
+    public Task<List<ProgramDTO>> GetProgramsAsync();
+}
---
+++ KlockanAPI/src/KlockanAPI.Application/Services/ProgramService.cs
@@ -0,0 +1,26 @@
+﻿using Mapster;
+using MapsterMapper;
+using KlockanAPI.Application.DTOs.Program;
+using KlockanAPI.Application.Services.Interfaces;
+
+namespace KlockanAPI.Application.Services;
+
+public class ProgramService : IProgramService
+{
+    private readonly IProgramRepository _programRepository;
+    private readonly IMapper _mapper;
+
+    public ProgramService(IProgramRepository programRepository, IMapper mapper)
+    {
+        this._programRepository = programRepository;
+        this._mapper = mapper;
+    }
+
+    public async Task<List<ProgramDTO>> GetProgramsAsync()
+    {
+        var result = await _programRepository.GetProgramsAsync();
+        var resultDTO = _mapper.Map<List<ProgramDTO>>(result);
+
+        return resultDTO;
+    }
+}
