b75422ea0fdbb15535fc5ac1d0950846977939ad
Andres J. Nazzari <andres.nazzari@jala.university>
2024-01-23 22:06:50 -0300

    Add ProgramRepository class with GetProgramsAsync method  TG-60

---
+++ KlockanAPI/src/KlockanAPI.Infrastructure/Repositories/ProgramRepository.cs
@@ -0,0 +1,43 @@
+﻿using KlockanAPI.Domain.Models;
+using KlockanAPI.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using KlockanAPI.Infrastructure.Data;
+
+namespace KlockanAPI.Infrastructure.Repositories;
+public class ProgramRepository : IProgramRepository
+{
+    private readonly KlockanContext _context;
+
+    public ProgramRepository(KlockanContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Program>> GetProgramsAsync()
+    {
+        return await Task.FromResult(_context.Programs.ToList());
+
+        // eliminar lo de aca abajo
+        var programs = new List<Program>()
+        {
+            new Program
+            {
+                Id = 1,
+                Name = "Bootcamp Developers 01",
+                Description = "Program covering concepts in software development.",
+                CreatedAt = new DateTime(2024, 1, 23, 0, 0, 0, DateTimeKind.Utc),
+                UpdatedAt = new DateTime(2024, 1, 23, 0, 0, 0, DateTimeKind.Utc)
+            },
+            new Program
+            {
+                Id = 2,
+                Name = "Advanced Bootcamp Developers 01",
+                Description = "Program focused on advanced software design and development techniques.",
+                CreatedAt = new DateTime(2024, 1, 23, 0, 0, 0, DateTimeKind.Utc),
+                UpdatedAt = new DateTime(2024, 1, 23, 0, 0, 0, DateTimeKind.Utc)
+            },
+        };
+
+        return await Task.FromResult(programs.ToList());
+    }
+}
