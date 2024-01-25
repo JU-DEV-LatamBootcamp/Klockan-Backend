61618b9b9244e43e8dcf6b5f53ec200ed7bbb1f7
Andres Javier Nazzari <151545133+AndresJavierNazzari@users.noreply.github.com>
2024-01-25 12:03:07 -0300

    Feature/tg 60 lists programs (#7)
    * Delete .gitkeep files TG-60
    * Update KlockanAPI.Application.csproj
    * Update project references and package versions
    * Add ProgramDTO class  TG-60
    * Add ProgramRepository class with GetProgramsAsync method  TG-60
    * Add IProgramRepository interface TG-60
    * Add ProgramController with GET endpoint for retrieving programs TG-60
    * Add IProgramService and ProgramService implementations TG-60
    * Add application service registration TG-60
    * Add infrastructure service registration TG-60
    * Refactor API configuration and add versioning, compression, and health checks TG-60
    * Delete application and infrastructure service registration files TG-60
    * Add application and infrastructure service registrations TG-60
    * Refactor ProgramService constructor and variable names TG-60
    * Remove unused code from ProgramRepository TG-60
    * Add IProgramRepository interface TG-60
    * Delete IProgramRepository interface TG-60
    * Add project reference to KlockanAPI.Infrastructure.csproj TG-60
    * Refactor program service and repository to use IEnumerable instead of List TG-60
    * Refactor program service and program repository
    * This commit refactors the `ProgramService` and `ProgramRepository` classes. The `GetAllAsync` method in the `ProgramService` class has been renamed to `GetAllProgramsAsync` for clarity. Similarly, the `GetProgramsAsync` method in the `ProgramRepository` class has been renamed to `GetAllProgramsAsync`. These changes improve code readability and maintainability. o unos
    * Remove unused using statements TG-60
    * Update database password TG-60
    * Comment redirection  TG-60
    * Fix Typo in programs variable and return TG-60
    * Create base project for KlockanAPI.Application.Test TG-60
    * Create base project for KlockanAPI.Presentation.Test TG-60
    * Add base projects for Application and presentation TG-60
    * Create GetAll test for ProgrmRepository
    * Fix Typo TG-60
    * Delete empty unit test files TG-60
    * Add KlockanAPI.Infraestructure.Tests project to solution TG-60
    * Update KlockanAPI.Application.Tests.csproj dependencies TG-60
    * Update KlockanAPI.Presentation.Tests.csproj dependencies TG-60
    * Add ProgramServiceTests for GetAllProgramsAsync TG-60
    * Add ProgramsControllerTests with GetAllPrograms_ShouldReturnOk test TG-60
    * Remove unused constructor  TG-60

---
+++ KlockanAPI/tests/KlockanAPI.Presentation.Tests/Controllers/ProgramsControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using NSubstitute;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+
+using KlockanAPI.Application.Services.Interfaces;
+using KlockanAPI.Application.DTOs.Program;
+using KlockanAPI.Presentation.Controllers;
+
+namespace KlockanAPI.Presentation.Tests.Controllers;
+
+public class ProgramsControllerTests
+{
+    private readonly IProgramService _programService;
+
+    public ProgramsControllerTests()
+    {
+        _programService = Substitute.For<IProgramService>();
+    }
+    private ProgramController GetControllerInstance() => new(_programService);
+
+    [Fact]
+    public async Task GetAllPrograms_ShouldReturnOk()
+    {
+        // Arrange
+        _programService.GetAllProgramsAsync().Returns(new List<ProgramDTO>());
+        var controller = GetControllerInstance();
+
+        // Act
+        var result = await controller.GetAllPrograms();
+
+        // Verify that the result is an ActionResult<IEnumerable<ProgramDTO>>
+        result.Should().BeOfType<ActionResult<IEnumerable<ProgramDTO>>>();
+
+        // Verify that the Result of the ActionResult is an OkObjectResult
+        result.Result.Should().BeOfType<OkObjectResult>();
+
+        // Verify the status code
+        (result?.Result as OkObjectResult)?.StatusCode.Should().Be(200);
+    }
+}
