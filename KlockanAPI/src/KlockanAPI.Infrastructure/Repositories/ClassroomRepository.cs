7b79d44bdb4f4a1b356513d11c91624c92862147
Andres Nazzari <andres.nazzari@jala.university>
2024-01-31 14:38:56 -0300

    Add ClassroomRepository implementation TG-93

---
+++ KlockanAPI/src/KlockanAPI.Infrastructure/Repositories/ClassroomRepository.cs
@@ -0,0 +1,24 @@
+﻿using KlockanAPI.Domain.Models;
+using KlockanAPI.Infrastructure.Data;
+using KlockanAPI.Infrastructure.Repositories;
+using KlockanAPI.Infrastructure.Repositories.Interfaces;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace KlockanAPI.Infrastructure.Repositories;
+
+public class ClassroomRepository : IClassroomRepository
+{
+    private readonly KlockanContext _context;
+
+    public ClassroomRepository(KlockanContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Classroom>?> GetClassroomsByCourseIdAsync(int courseId)
+    {
+        var classrooms = await _context.Classrooms.Where(c => c.CourseId == courseId).ToListAsync();
+        return classrooms.Count > 0 ? classrooms : null;
+    }
+}
