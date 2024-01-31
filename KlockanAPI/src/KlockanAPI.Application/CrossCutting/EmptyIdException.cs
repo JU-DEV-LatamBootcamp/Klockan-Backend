15f5b320a0c6738af3f3755274e5915e2980811f
Andres Nazzari <andres.nazzari@jala.university>
2024-01-31 12:47:08 -0300

    Add EmptyIdException class to handle empty IDs TG-93

---
+++ KlockanAPI/src/KlockanAPI.Application/CrossCutting/EmptyIdException.cs
@@ -0,0 +1,17 @@
+﻿namespace KlockanAPI.Application.CrossCutting;
+
+public class EmptyIdException : ArgumentException
+{
+    public EmptyIdException(string message) : base(message)
+    {
+    }
+
+    public static void ThrowIfEmpty(
+        int id, string? message = null, string? elementName = null)
+    {
+        if(id == 0)
+        {
+            throw new EmptyIdException(message ?? $"{elementName} Id cannot be empty");
+        }
+    }
+}
