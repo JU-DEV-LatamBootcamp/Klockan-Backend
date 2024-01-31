559cfb8a6ab5b5f74cbfc1341b70a1306d99be37
Andres Nazzari <andres.nazzari@jala.university>
2024-01-31 12:47:17 -0300

    Add NotFoundException class for handling resource not found exceptions TG-93

---
+++ KlockanAPI/src/KlockanAPI.Application/CrossCutting/NotFoundException.cs
@@ -0,0 +1,14 @@
+﻿namespace KlockanAPI.Application.CrossCutting;
+
+public class NotFoundException : ArgumentException
+{
+    public NotFoundException(string? message) : base(message) { }
+
+    public static void ThrowIfNull<T>(T? param, string? message = null)
+    {
+        if(param is null)
+        {
+            throw new NotFoundException(message ?? "Resource was not found");
+        }
+    }
+}
