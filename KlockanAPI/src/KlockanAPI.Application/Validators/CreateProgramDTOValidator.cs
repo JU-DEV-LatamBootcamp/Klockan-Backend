71aaa825b49e0d534f955d2c4714a337b27e9008
Adrian Mendoza Mamani <adrian.mendoza@fundacion-jala.org>
2024-02-01 01:36:44 -0400

    TG-86: Add Validator for Create Program

---
+++ KlockanAPI/src/KlockanAPI.Application/Validators/CreateProgramDTOValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using KlockanAPI.Application.DTOs.Program;
+
+namespace KlockanAPI.Application.Validators;
+
+public class CreateProgramDTOValidator : AbstractValidator<CreateProgramDTO>
+{
+    public CreateProgramDTOValidator()
+    {
+        RuleFor(p => p.Name)
+            .NotEmpty().WithMessage("The name is required.")
+            .Length(1, 200).WithMessage("The name must be between 1 and 200 characters.");
+    }
+}
+
