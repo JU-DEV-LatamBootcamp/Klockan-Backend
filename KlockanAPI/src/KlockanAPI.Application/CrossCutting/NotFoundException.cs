37f4e663cd714c849b515272eaa5e789decf9dd4
Andres Nazzari <andres.nazzari@jala.university>
2024-02-01 11:20:23 -0300

    Update NotFoundException constructor to provide a default message TG-93

--- KlockanAPI/src/KlockanAPI.Application/CrossCutting/NotFoundException.cs
+++ KlockanAPI/src/KlockanAPI.Application/CrossCutting/NotFoundException.cs
@@ -2,13 +2,13 @@
 
 public class NotFoundException : ArgumentException
 {
-    public NotFoundException(string? message) : base(message) { }
+    public NotFoundException(string? message = "Resource was not found") : base(message) { }
 
     public static void ThrowIfNull<T>(T? param, string? message = null)
     {
         if(param is null)
         {
-            throw new NotFoundException(message ?? "Resource was not found");
+            throw new NotFoundException(message);
         }
     }
 }
