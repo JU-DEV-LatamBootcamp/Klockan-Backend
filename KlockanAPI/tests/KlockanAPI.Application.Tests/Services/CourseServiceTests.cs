5b93bb1b4422b0186076361d6df3c7a9d83562c3
RamiroJGuillen <ramiro.guillen@jala.university>
2024-02-01 13:54:36 -0300

    TG-84: updated tests to remove code from courses

--- KlockanAPI/tests/KlockanAPI.Application.Tests/Services/CourseServiceTests.cs
+++ KlockanAPI/tests/KlockanAPI.Application.Tests/Services/CourseServiceTests.cs
@@ -33,7 +33,6 @@ public class CourseServiceTests
             {
                 Id = 1,
                 Name = "Frontend Development",
-                Code = "FE",
                 Description = "Course to develop Web Applications focusing on HTML, CSS, JavaScript, and popular frameworks.",
                 Sessions = 10,
                 SessionDuration = 60,
@@ -43,7 +42,6 @@ public class CourseServiceTests
             {
                 Id = 2,
                 Name = "Backend Development",
-                Code = "BE",
                 Description = "Course on server side programming, databases, and API construction.",
                 Sessions = 12,
                 SessionDuration = 75,
@@ -53,7 +51,6 @@ public class CourseServiceTests
             {
                 Id = 3,
                 Name = "Full Stack Development",
-                Code = "FS",
                 Description = "Comprehensive course covering both frontend and backend development to build complete applications.",
                 Sessions = 15,
                 SessionDuration = 90,
