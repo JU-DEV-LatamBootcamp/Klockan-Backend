fa9566ab034b0403ec4faeb241ab699b73bf5b6d
Adrian-Mendoza-Jala <151578320+Adrian-Mendoza-Jala@users.noreply.github.com>
2024-02-01 15:20:08 -0400

    Feature/tg 86 create program (#13)
    * TG-86: Update Repository with Create Program
    
    * TG-86: Add DTO for Create Program
    
    * TG-86: Update Services for Create Program
    
    * TG-86: Add Validator for Create Program
    
    * TG-86: Add Endpoint and Fluent Validation for Create Program
    
    * TG-86: Add Tests for Create Program Service
    
    * TG-86: Add Tests for Create Program Repository
    
    * TG-86: Add Tests for Create Program Controller
    
    ---------
    
    Co-authored-by: Adrian Mendoza Mamani <adrian.mendoza@fundacion-jala.org>
    Co-authored-by: Andres Javier Nazzari <151545133+AndresJavierNazzari@users.noreply.github.com>

---
+++ KlockanAPI/src/KlockanAPI.Application/DTOs/Program/CreateProgramDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace KlockanAPI.Application.DTOs.Program;
+
+public class CreateProgramDTO
+{
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+}
+
