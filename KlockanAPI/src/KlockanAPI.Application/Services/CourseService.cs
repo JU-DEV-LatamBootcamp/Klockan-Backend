9910b6f9e22ff3c8b69e5601c5b0f2521c8a20cd
Andres Javier Nazzari <151545133+AndresJavierNazzari@users.noreply.github.com>
2024-02-01 16:16:39 -0300

    Feature/tg 93 course delete (#12)
    * Add EmptyIdException class to handle empty IDs TG-93
    * Add NotFoundException class for handling resource not found exceptions TG-93
    * Add DeleteCourseAsync method to CourseRepository TG-93
    * Add DeleteCourseAsync method to CourseService TG-93
    * Add GlobalExceptionHandler middleware TG-93
    * Add middleware for exception handling and problem details TG-93
    * Delete unused CrossCutting and Middlewares folders TG-93
    * Add FoundException class for handling resource not found exceptions TG-93
    * Add exception handling for FoundException TG-93
    * Refactor CourseRepository and ICourseRepository  TG-93
    * This commit refactors the CourseRepository and ICourseRepository classes. It replaces the DeleteCourseAsync method with GetCourseByIdAsync and DeleteCourseAsync methods. The changes improve code readability and maintainability.
    * Add Delete Course API endpoint  TG-93
    * Refactor DeleteCourseAsync method in CourseService  TG-93
    * Remove unused files and folders TG-93
    * Add exception handling middleware TG-93
    * Add IClassroomRepository interface TG-93
    * Add ClassroomRepository implementation TG-93
    * Refactor exception handling in GlobalExceptionHandler.cs TG-93
    * Add classroomRepository dependency to CourseService constructor TG-93
    * Remove check for course usage in classroom TG-93
    * Fix formatting and add missing using statement in CourseService.cs TG-93
    * Update Classroom model properties TG-93
    * Refactor ClassroomRepository imports TG-93
    * Fix formatting in CourseRepository.cs TG-93
    * Add delete course tests TG-93
    * Add Delete_ShouldReturnOk test case to CoursesControllerTests TG-93
    * Refactor CourseRepositoryTests and add new test case for DeleteCourseAsync TG-93
    * Add ClassroomRepositoryTests for GetClassroomsByCourseIdAsync method TG-93
    * Remove unnecessary line in ClassroomRepositoryTests.cs TG-93
    * Refactor ThrowIfEmpty method to ThrowIfIdZero TG-93
    * Refactor FoundException constructor to provide a default message TG-93
    * Update NotFoundException constructor to provide a default message TG-93
    * Change comment language  TG-93
    * Fix formatting issues in interface and test files  TG-93

--- KlockanAPI/src/KlockanAPI.Application/Services/CourseService.cs
+++ KlockanAPI/src/KlockanAPI.Application/Services/CourseService.cs
@@ -1,18 +1,22 @@
 ﻿using MapsterMapper;
+
 using KlockanAPI.Application.DTOs.Course;
 using KlockanAPI.Application.Services.Interfaces;
 using KlockanAPI.Infrastructure.Repositories.Interfaces;
+using KlockanAPI.Application.CrossCutting;
 
 namespace KlockanAPI.Application.Services;
 
 public class CourseService : ICourseService
 {
     private readonly ICourseRepository _courseRepository;
+    private readonly IClassroomRepository _classroomRepository;
     private readonly IMapper _mapper;
 
-    public CourseService(ICourseRepository courseRepository, IMapper mapper)
+    public CourseService(ICourseRepository courseRepository, IClassroomRepository classroomRepository, IMapper mapper)
     {
         _courseRepository = courseRepository;
+        _classroomRepository = classroomRepository;
         _mapper = mapper;
     }
 
@@ -20,5 +24,18 @@ public class CourseService : ICourseService
     {
         var courses = await _courseRepository.GetAllAsync();
         return _mapper.Map<IEnumerable<CourseDto>>(courses);
+    }
+    public async Task<CourseDto?> DeleteCourseAsync(int id)
+    {
+        var course = await _courseRepository.GetCourseByIdAsync(id);
+        NotFoundException.ThrowIfNull(course, $"Course with id {id} not found");
+
+        var classroom = await _classroomRepository.GetClassroomsByCourseIdAsync(id);
+        FoundException.ThrowIfNotNull(classroom, $"Course with id {id} is used in a classroom");
+
+        var deletedCourse = await _courseRepository.DeleteCourseAsync(course!);
+
+
+        return _mapper.Map<CourseDto>(deletedCourse);
     }
 }
---
+++ KlockanAPI/src/KlockanAPI.Infrastructure/Repositories/Interfaces/IClassroomRepository.cs
@@ -0,0 +1,8 @@
+﻿using KlockanAPI.Domain.Models;
+
+namespace KlockanAPI.Infrastructure.Repositories.Interfaces;
+
+public interface IClassroomRepository
+{
+    Task<IEnumerable<Classroom>?> GetClassroomsByCourseIdAsync(int courseId);
+}
