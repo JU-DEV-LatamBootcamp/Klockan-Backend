ae14c7651676956741a7f381e90bf23c632edafd
RamiroJGuillen <ramiro.guillen@jala.university>
2024-02-02 13:54:51 -0300

    made description property as optional

--- KlockanAPI/src/KlockanAPI.Application/DTOs/Course/CourseDto.cs
+++ KlockanAPI/src/KlockanAPI.Application/DTOs/Course/CourseDto.cs
@@ -4,7 +4,7 @@ public class CourseDTO
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
+    public string? Description { get; set; }
     public int? Sessions { get; set; }
     public int? SessionDuration { get; set; }
 }
