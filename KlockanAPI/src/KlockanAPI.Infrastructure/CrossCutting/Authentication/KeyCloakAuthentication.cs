f0959e45d3a6bb4209da9574937519d230192bc3
Andres J. Nazzari <lordcliff@gmail.com>
2024-02-08 11:49:30 -0300

    IssuerSigningKey

--- KlockanAPI/src/KlockanAPI.Infrastructure/CrossCutting/Authentication/KeyCloakAuthentication.cs
+++ KlockanAPI/src/KlockanAPI.Infrastructure/CrossCutting/Authentication/KeyCloakAuthentication.cs
@@ -31,8 +31,8 @@ public static class KeyCloakAuthentication
                 ValidateIssuer = true,
                 ValidIssuer = KeyCloakSecrets["Authority"],
                 ValidateIssuerSigningKey = true,
-                IssuerSigningKey = new JsonWebKey(KeyCloakSecrets["CertsUrl"]),
-                //IssuerSigningKey = new JsonWebKey(KeyCloakSecrets["Certs"]),
+                //IssuerSigningKey = new JsonWebKey(KeyCloakSecrets["CertsUrl"]),
+                IssuerSigningKey = new JsonWebKey(KeyCloakSecrets["Certs"]),
 
             };
             o.TokenValidationParameters = tokenValidationParameters;
