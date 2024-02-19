a79bad802ab368d54bcd127d3f2c2341ede78e95
Andres Nazzari <andres.nazzari@jala.university>
2024-02-19 08:46:57 -0300

    Add UsersController with GetAllUsers endpoint TG-89

---
+++ KlockanAPI/src/KlockanAPI.Presentation/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+using KlockanAPI.Application.DTOs.User;
+using KlockanAPI.Application.Services.Interfaces;
+
+namespace KlockanAPI.Presentation.Controllers;
+
+[ApiController]
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiVersion("1.0")]
+public class UsersController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UsersController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpGet]
+    [HttpHead]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<IEnumerable<UserDto>>> GetAllUsers()
+    {
+        try {
+            var users = await _userService.GetAllUsersAsync();
+            return Ok(users);
+        } catch(Exception ex){
+            return StatusCode(500, $"Internal server error: {ex.Message}");
+        }
+    }
+}
