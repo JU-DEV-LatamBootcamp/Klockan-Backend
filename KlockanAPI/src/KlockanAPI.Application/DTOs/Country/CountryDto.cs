0d22b8825ffc7bbc8423e247a42be3ec7b8082ca
Andres Nazzari <andres.nazzari@jala.university>
2024-02-19 11:46:30 -0300

    Update City and Country DTOs and Models

--- KlockanAPI/src/KlockanAPI.Application/DTOs/Country/CountryDto.cs
+++ KlockanAPI/src/KlockanAPI.Application/DTOs/Country/CountryDto.cs
@@ -5,5 +5,5 @@ public class CountryDto
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    public string? Code { get; set; } = string.Empty;
+    public string? Code { get; set; }
 }
--- KlockanAPI/src/KlockanAPI.Domain/Models/City.cs
+++ KlockanAPI/src/KlockanAPI.Domain/Models/City.cs
@@ -3,7 +3,7 @@ namespace KlockanAPI.Domain.Models;
 public class City : BaseModel
 {
     public string Name { get; set; } = string.Empty;
-    public string? Code { get; set; } = string.Empty;
+    public string? Code { get; set; }
     public int CountryId { get; set; }
     public Country? Country { get; set; }
     public ICollection<User> Users { get; set; } = new List<User>();
