a43066bee56c1db6cdbd3374fe1956cca6791d3e
Andres Nazzari <andres.nazzari@jala.university>
2024-02-19 08:45:32 -0300

    Create Add Avatar to user and seeders migration TG-89

--- KlockanAPI/src/KlockanAPI.Infrastructure/Data/Migrations/20240216163410_Addd_Avatar_to_Users_and_Seeders.Designer.cs
+++ KlockanAPI/src/KlockanAPI.Infrastructure/Data/Migrations/20240216163410_Addd_Avatar_to_Users_and_Seeders.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using KlockanAPI.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace KlockanAPI.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(KlockanContext))]
-    partial class KlockanContextModelSnapshot : ModelSnapshot
+    [Migration("20240216163410_Addd_Avatar_to_Users_and_Seeders")]
+    partial class Addd_Avatar_to_Users_and_Seeders
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
