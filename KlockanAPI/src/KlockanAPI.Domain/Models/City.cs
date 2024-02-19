4318a71c12d1320497d101d30442ddb075a9752d
Andres Nazzari <andres.nazzari@jala.university>
2024-02-19 08:46:05 -0300

    Update City model to allow nullable Country property TG-89

--- KlockanAPI/src/KlockanAPI.Domain/Models/City.cs
+++ KlockanAPI/src/KlockanAPI.Domain/Models/City.cs
@@ -5,6 +5,6 @@ public class City : BaseModel
     public string Name { get; set; } = string.Empty;
     public string? Code { get; set; } = string.Empty;
     public int CountryId { get; set; }
-    public Country Country { get; set; } = new Country();
+    public Country? Country { get; set; }
     public ICollection<User> Users { get; set; } = new List<User>();
 }
