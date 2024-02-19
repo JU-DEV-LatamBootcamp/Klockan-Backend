efd4a70c77251207578164a8d1fd4179d06200d4
Andres Nazzari <andres.nazzari@jala.university>
2024-02-19 08:47:20 -0300

    Add user mapping configuration and registration TG-89

---
+++ KlockanAPI/src/KlockanAPI.Application/Mappings/MappingRegistrations.cs
@@ -0,0 +1,16 @@
+﻿using Mapster;
+using MapsterMapper;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KlockanAPI.Application.Mappings;
+
+public static class MappingRegistrations
+{
+    public static IServiceCollection AddMappingRegistrations(this IServiceCollection services)
+    {
+        MappingConfigUser.ConfigureMappingUserUserDto();
+
+        return services;
+    }
+}
