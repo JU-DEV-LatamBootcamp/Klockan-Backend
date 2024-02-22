8f6355cafab5749db11076787e50211b2d4c9b28
Andres Javier Nazzari <151545133+AndresJavierNazzari@users.noreply.github.com>
2024-02-22 11:10:59 -0300

    Feature/tg129 tests (#39)
    * TG-123: Remove useless files
    * TG-123: Update Gitignore for Test Coverage
    * TG-123: Fix Failed Tests
    * TG-123: Add config for Test Coverage
    * TG-123: Add documentation for Test Coverage
    * Add integration tests
    * Update code coverage instructions and command summary
    * Fix formatting in ClassroomServiceTests.cs
    * Add test for EditProgramAsync method
    * Add additional tests for ClassroomRepository
    * Refactor CountryRepositoryTests class and add Dispose method
    * Refactor CourseRepositoryTests and add new test cases
    * Refactor MeetingRepositoryTests and add new test cases
    * Refactor ProgramRepositoryTests class and add new test methods
    * Refactor UserRepositoryTests class and add Dispose method
    * Add unit tests for EditProgram method
    * Refactor KlockanApiFactory.cs***
    ***Update KlockanApiFactory.cs with necessary changes
    * Remove empty file

--- KlockanAPI/tests/KlockanAPI.Infraestructure.Tests/Repositories/ProgramRepositoryTests.cs
+++ KlockanAPI/tests/KlockanAPI.Infraestructure.Tests/Repositories/ProgramRepositoryTests.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace KlockanAPI.Infraestructure.Tests.Repositories;
 
-public class ProgramRepositoryTests
+public class ProgramRepositoryTests : IDisposable
 {
     private readonly KlockanContext _context;
 
@@ -20,6 +20,12 @@ public class ProgramRepositoryTests
         _context = new KlockanContext(dbContextOptions.Options);
     }
     private ProgramRepository GetRepositoryInstance() => new(_context);
+
+    public void Dispose()
+    {
+        // Make sure that the in-memory database is deleted at the end of all tests.
+        _context.Database.EnsureDeleted();
+    }
 
     [Fact]
     public async Task GetAllProgramsAsync_ShouldReturnPrograms()
@@ -76,27 +82,19 @@ public class ProgramRepositoryTests
     public async Task CreateProgramAsync_ShouldAddProgram_WhenCalled()
     {
         // Arrange
-        var options = new DbContextOptionsBuilder<KlockanContext>()
-            .UseInMemoryDatabase(databaseName: "ProgramRepositoryDB")
-            .Options;
 
-        using (var context = new KlockanContext(options))
-        {
-            var repository = new ProgramRepository(context);
+        var repository = new ProgramRepository(_context);
 
-            var newProgram = new Program
-            {
-                // Configura las propiedades necesarias de tu modelo
-            };
+        var newProgram = new Program { };
 
-            // Act
-            var result = await repository.CreateProgramAsync(newProgram);
+        // Act
+        var result = await repository.CreateProgramAsync(newProgram);
 
-            // Assert
-            Assert.NotNull(result);
-            var programInDb = await context.Programs.FindAsync(result.Id);
-            Assert.NotNull(programInDb);
-        }
+        // Assert
+        Assert.NotNull(result);
+        var programInDb = await _context.Programs.FindAsync(result.Id);
+        Assert.NotNull(programInDb);
+
     }
 
     [Fact]
@@ -123,4 +121,83 @@ public class ProgramRepositoryTests
         Assert.Equal(program, result);
     }
 
+    [Fact]
+    public async Task GetProgramByIdAsync_WhenProgramExists_ReturnsProgram()
+    {
+        // Arrange
+        var existingProgramId = 1;
+        var existingProgram = new Program
+        {
+            Id = existingProgramId,
+            Name = "Existing Program",
+            Description = "Description of existing program"
+        };
+        _context.Programs.Add(existingProgram);
+        await _context.SaveChangesAsync();
+
+        var programRepository = GetRepositoryInstance();
+
+        // Act
+        var result = await programRepository.GetProgramByIdAsync(existingProgramId);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType<Program>();
+        result!.Id.Should().Be(existingProgramId);
+        result.Name.Should().Be(existingProgram.Name);
+        result.Description.Should().Be(existingProgram.Description);
+    }
+
+    [Fact]
+    public async Task GetProgramByIdAsync_WhenProgramDoesNotExist_ReturnsNull()
+    {
+        // Arrange
+        var nonExistingProgramId = 999;
+        var programRepository = GetRepositoryInstance();
+
+        // Act
+        var result = await programRepository.GetProgramByIdAsync(nonExistingProgramId);
+
+        // Assert
+        result.Should().BeNull();
+    }
+
+    [Fact]
+    public async Task EditProgramAsync_WhenProgramExists_EditsAndReturnsEditedProgram()
+    {
+        // Arrange
+        var existingProgramId = 1;
+        var existingProgram = new Program
+        {
+            Id = existingProgramId,
+            Name = "Existing Program",
+            Description = "Description of existing program"
+        };
+        _context.Programs.Add(existingProgram);
+        await _context.SaveChangesAsync();
+
+        var editedProgram = new Program
+        {
+            Id = existingProgramId,
+            Name = "Edited Program",
+            Description = "Description of edited program"
+        };
+        ProgramRepository? programRepository = GetRepositoryInstance();
+
+        // Act
+        var result = await programRepository.EditProgramAsync(editedProgram);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType<Program>();
+        result.Id.Should().Be(existingProgramId);
+        result.Name.Should().Be(editedProgram.Name);
+        result.Description.Should().Be(editedProgram.Description);
+
+        // Additional Assertion: Ensure Program is correctly edited in the database
+        var editedProgramFromDb = await _context.Programs.FindAsync(existingProgramId);
+        editedProgramFromDb.Should().NotBeNull();
+        editedProgramFromDb!.Name.Should().Be(editedProgram.Name);
+        editedProgramFromDb.Description.Should().Be(editedProgram.Description);
+    }
 }
