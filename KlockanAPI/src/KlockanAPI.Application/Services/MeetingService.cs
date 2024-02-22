81dc316caa1b3ade356807e09552f399c4a6fd25
carlosBarriosCarvajal <Carlos.Barrios@jala.university>
2024-02-22 10:54:15 -0500

    TG-100 Create single meeting service interface

--- KlockanAPI/src/KlockanAPI.Application/Services/MeetingService.cs
+++ KlockanAPI/src/KlockanAPI.Application/Services/MeetingService.cs
@@ -2,6 +2,8 @@ using MapsterMapper;
 using KlockanAPI.Application.DTOs.Meeting;
 using KlockanAPI.Application.Services.Interfaces;
 using KlockanAPI.Infrastructure.Repositories.Interfaces;
+using KlockanAPI.Domain.Models;
+using Microsoft.IdentityModel.Tokens;
 
 namespace KlockanAPI.Application.Services;
 public class MeetingService : IMeetingService
@@ -20,4 +22,33 @@ public class MeetingService : IMeetingService
         var meetings = await _meetingRepository.GetAllAsync();
         return _mapper.Map<IEnumerable<MeetingDto>>(meetings);
     }
+
+    public async Task<MeetingDto> CreateSingleMeeting(CreateMeetingDto createMeetingDto)
+    {
+        var meeting = _mapper.Map<Meeting>(createMeetingDto);
+        meeting.SessionNumber = await _meetingRepository.GetSessionNumber(meeting.ClassroomId) + 1;
+        meeting.CreatedAt = DateTime.UtcNow;
+        var createdMeeting = await _meetingRepository.CreateSingleMeeting(meeting);
+
+        if (!createMeetingDto.Users.IsNullOrEmpty() && createdMeeting != null)
+        {
+            var meetingAttendances = createMeetingDto.Users.Select(s => new MeetingAttendance
+            {
+                MeetingId = createdMeeting.Id,
+                ClassroomUserId = s,
+                CreatedAt = DateTime.UtcNow,
+                MeetingAttendanceStatusId = 2,
+                User = null,
+                Meeting = null,
+                Status = null
+            }).
+            ToList();
+
+            createdMeeting.MeetingAttendances = meetingAttendances;
+
+            await _meetingRepository.AssignStudents(createdMeeting.MeetingAttendances, createdMeeting.ClassroomId);
+        }
+        return _mapper.Map<MeetingDto>(createdMeeting);
+    }
+
 }
