eb68bdca7d74fc4cf9de2eb488a0afaa7dbd7262
carlosBarriosCarvajal <Carlos.Barrios@jala.university>
2024-02-22 18:11:12 -0500

    TG-100 Create meeting controller test

--- KlockanAPI/tests/KlockanAPI.Presentation.Tests/Controllers/MeetingsControllerTests.cs
+++ KlockanAPI/tests/KlockanAPI.Presentation.Tests/Controllers/MeetingsControllerTests.cs
@@ -5,6 +5,8 @@ using KlockanAPI.Application.Services.Interfaces;
 using KlockanAPI.Application.DTOs.Course;
 using KlockanAPI.Presentation.Controllers;
 using KlockanAPI.Application.DTOs.Meeting;
+using NSubstitute.ExceptionExtensions;
+using System.Net;
 
 namespace KlockanAPI.Presentation.Tests.Controllers;
 
@@ -74,5 +76,57 @@ public class MeetingsControllerTest
 
         var meetingsData = okResult.Value as IEnumerable<MeetingDto>;
         meetingsData.Should().BeEquivalentTo(sampleMeetings);
+    }
+
+    [Fact]
+    public async Task CreateMeeting_Return201Created_WithValidInput()
+    {
+        //Arrange
+        var createMeetingDto = new CreateMeetingDto
+        {
+            Date = new DateOnly(2024, 2, 22),
+            Time = new TimeOnly(14, 0, 0),
+            ClassroomId = 1,
+            TrainerId = 1,
+            Users = new List<int> { 1, 2, 3 }
+        };
+        var createdMeetingDto = new MeetingDto
+        {
+            Id = 1,
+            SessionNumber = 1,
+            Date = createMeetingDto.Date,
+            Time = createMeetingDto.Time,
+            ClassroomId = createMeetingDto.ClassroomId
+        };
+
+        _meetingService.CreateSingleMeeting(createMeetingDto).Returns(Task.FromResult(createdMeetingDto));
+        var controller = GetControllerInstance();
+
+        //Act
+        var result = await controller.CreateMeeting(createMeetingDto);
+        //Assert
+        var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(result.Result);
+        var model = Assert.IsType<MeetingDto>(createdAtActionResult.Value);
+        Assert.Equal(createdMeetingDto, model);
+    }
+
+    [Fact]
+    public async Task CreateMeeting_Returns400BadRequest_WithInvalidInput()
+    {
+        // Arrange
+        var controller = GetControllerInstance();
+        var createMeetingDto = new CreateMeetingDto
+        {
+            Date = new DateOnly(2024, 2, 22),
+            Time = new TimeOnly(14, 0, 0),
+        };
+
+        controller.ModelState.AddModelError("PropertyName", "Error message");
+
+        // Act
+        var result = await controller.CreateMeeting(createMeetingDto);
+
+        // Assert
+        Assert.IsType<BadRequestObjectResult>(result.Result);
     }
 }
