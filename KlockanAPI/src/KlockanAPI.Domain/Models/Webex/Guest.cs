cedebd78eccc5d28137ec63fb36884f3040e2346
Adrian Mendoza Mamani <adrian.mendoza@fundacion-jala.org>
2024-03-04 08:31:35 -0400

    TG-147: Add Models for Webex

---
+++ KlockanAPI/src/KlockanAPI.Domain/Models/Webex/Guest.cs
@@ -0,0 +1,9 @@
+﻿namespace KlockanAPI.Domain.Models.Webex;
+
+public class Guest
+{
+    public string email;
+    public string displayName;
+    public bool coHost;
+    public bool panelist;
+}
---
+++ KlockanAPI/src/KlockanAPI.Domain/Models/Webex/Meeting.cs
@@ -0,0 +1,13 @@
+﻿namespace KlockanAPI.Domain.Models.Webex;
+
+public class Meeting
+{
+    public string title;
+    public string start;
+    public string end;
+    public string timezone;
+    public string recurrence;
+    public List<Guest> invitees;
+    public bool sendEmail;
+    public string hostEmail;
+}
