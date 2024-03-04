bccd6cb8dbf366b1567547bfb3b384c37f424309
Adrian Mendoza Mamani <adrian.mendoza@fundacion-jala.org>
2024-03-04 08:33:28 -0400

    TG-147: Add Config and Dependency Injection for Services

--- KlockanAPI/src/KlockanAPI.Presentation/Registrations/ApplicactionServiceRegistration.cs
+++ KlockanAPI/src/KlockanAPI.Presentation/Registrations/ApplicactionServiceRegistration.cs
@@ -2,20 +2,35 @@ using System.Reflection;
 using Mapster;
 using MapsterMapper;
 using FluentValidation;
+using Microsoft.Extensions.Options;
 
 using KlockanAPI.Application.Services;
 using KlockanAPI.Application.Services.Interfaces;
 using KlockanAPI.Application.Validators;
+using KlockanAPI.Application.Services.Webex;
+using KlockanAPI.Domain.Models.Webex;
 
 
 namespace KlockanAPI.Application;
 public static class ApplicactionServiceRegistration
 {
-    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
     {
         // Mapster configuration, this scans all custom configs
         var config = TypeAdapterConfig.GlobalSettings;
         config.Scan(Assembly.GetExecutingAssembly());
+
+        // Bind Webex options
+        var webexOptionsSection = configuration.GetSection("Webex");
+        services.Configure<WebexOptions>(webexOptionsSection);
+
+        // Register WebexService with HttpClient configured
+        services.AddHttpClient<WebexService>(client =>
+        {
+            client.BaseAddress = new Uri(configuration["Webex:MeetingsApiUrl"]);
+            client.DefaultRequestHeaders.Authorization =
+                new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", configuration["Webex:AccessToken"]);
+        });
 
         services
             .AddScoped<IProgramService, ProgramService>()
@@ -23,12 +38,15 @@ public static class ApplicactionServiceRegistration
             .AddScoped<IClassroomService, ClassroomService>()
             .AddScoped<IMeetingService, MeetingService>()
             .AddScoped<IScheduleService, ScheduleService>()
-
             .AddScoped<IUserService, UserService>()
             .AddScoped<ICountryService, CountryService>()
+            .AddSingleton<WebexMeetingAdapter>()
+            .AddSingleton<IThirdPartyMeeting>(serviceProvider =>
+                MeetingServiceFactory.CreateMeetingService("Webex", serviceProvider))
             .AddSingleton(config)
             .AddScoped<IMapper, ServiceMapper>();
 
+        // Register validators
         services
             .AddValidatorsFromAssemblyContaining<CreateCourseDTOValidator>()
             .AddValidatorsFromAssemblyContaining<CreateProgramDTOValidator>()
