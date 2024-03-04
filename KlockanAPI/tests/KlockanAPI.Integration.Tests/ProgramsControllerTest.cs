13e2edda61a426585000b7bcd0714dfa370437f5
Andres J. Nazzari <andres.nazzari@jala.university>
2024-03-04 17:32:01 -0300

    Add authorization header to GetPrograms integration test

--- KlockanAPI/tests/KlockanAPI.Integration.Tests/ProgramsControllerTest.cs
+++ KlockanAPI/tests/KlockanAPI.Integration.Tests/ProgramsControllerTest.cs
@@ -1,27 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 
+using KlockanAPI.Domain.Keycloak;
 using KlockanAPI.Domain.Models;
+using KlockanAPI.Integration.Tests.Helpers.Keycloak;
+
 
 namespace KlockanAPI.Integration.Tests;
 
 public class ProgramsControllerTest : IClassFixture<KlockanApiFactory>
 {
     public readonly HttpClient _client;
+    private readonly AuthServiceHelper _authServiceHelper;
+
     public ProgramsControllerTest(KlockanApiFactory factory)
     {
         factory.ClientOptions.BaseAddress = new Uri("https://localhost:5001/api/v1/programs");
         _client = factory.CreateClient();
+        _authServiceHelper = new AuthServiceHelper();
     }
 
     [Fact]
     [Trait("Category", "Integration")]
     public async Task GetPrograms_ReturnsOk()
     {
+        Token token = await _authServiceHelper.GetAdminToken();
+        _client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token.access_token}");
+
         var response = await _client.GetStringAsync("");
         var programs = JsonSerializer.Deserialize<IEnumerable<Program>>(response);
         Console.WriteLine(response);
