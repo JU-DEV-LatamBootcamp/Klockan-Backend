300c06666953435b3ccf77ac649a2691b0d1c3c1
Andres J. Nazzari <andres.nazzari@jala.university>
2024-03-04 11:44:44 -0300

    Add User interface to Keycloak domain

---
+++ KlockanAPI/src/KlockanAPI.Domain/Keycloak/User.cs
@@ -0,0 +1,9 @@
+﻿
+namespace KlockanAPI.Domain.Keycloak;
+
+public interface User
+{
+    string username { get; set; }
+    bool enabled { get; set; }
+    Credential[] credentials { get; set; }
+}
