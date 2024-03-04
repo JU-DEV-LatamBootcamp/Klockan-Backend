e85a8d715b8f0d92051ef29dc73a7223c4986a6d
Andres J. Nazzari <andres.nazzari@jala.university>
2024-03-04 11:44:40 -0300

    Add constants for role IDs

--- KlockanAPI/src/KlockanAPI.Domain/Models/Role.cs
+++ KlockanAPI/src/KlockanAPI.Domain/Models/Role.cs
@@ -2,6 +2,9 @@ namespace KlockanAPI.Domain.Models;
 
 public class Role : BaseModel
 {
+    public static readonly int ADMIN_ID = 1;
+    public static readonly int TRAINER_ID = 2;
+
     public string Name { get; set; } = string.Empty;
     public ICollection<User> Users { get; set; } = new List<User>();
     public ICollection<ClassroomUser> ClassroomUsers { get; set; } = new List<ClassroomUser>();
