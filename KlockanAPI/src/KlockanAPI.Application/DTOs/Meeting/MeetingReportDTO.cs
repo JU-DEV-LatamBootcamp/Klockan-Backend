12ba041f7ac18e4cd3421c367e672aad1e975db1
jala-foundation-student22 <student22@jala-foundation.org>
2024-03-07 15:55:09 -0400

    Fix namespace block

--- KlockanAPI/src/KlockanAPI.Application/DTOs/Meeting/MeetingReportDTO.cs
+++ KlockanAPI/src/KlockanAPI.Application/DTOs/Meeting/MeetingReportDTO.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace KlockanAPI.Application.DTOs.Meeting
+namespace KlockanAPI.Application.DTOs.Meeting;
+
+public class MeetingReportDTO
 {
-    public class MeetingReportDTO
-    {
-        public List<MeetingParticipantReportDTO> items { get; set; }
-    }
+    public List<MeetingParticipantReportDTO> items { get; set; }
 }
