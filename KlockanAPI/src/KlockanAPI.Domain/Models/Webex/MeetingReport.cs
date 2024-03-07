b9250b4b7991f6a39d32ded6309493c1d8afb7b6
jala-foundation-student22 <student22@jala-foundation.org>
2024-03-07 15:54:07 -0400

    Fix namespace block in classes, changed connection string

--- KlockanAPI/src/KlockanAPI.Domain/Models/Webex/MeetingReport.cs
+++ KlockanAPI/src/KlockanAPI.Domain/Models/Webex/MeetingReport.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace KlockanAPI.Domain.Models.Webex
+namespace KlockanAPI.Domain.Models.Webex;
+
+public class MeetingReport
 {
-    public class MeetingReport
-    {
-        public List<ParticipantReport> items { get; set; }
-    }
+    public List<ParticipantReport> items { get; set; }
 }
